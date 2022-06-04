bc377d90ccceecaa958e1691bb03731a4804631c
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-06-05 00:10:21 +0530

    starting commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Text('Chart'),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
+      ),
+    );
+  }
+}
