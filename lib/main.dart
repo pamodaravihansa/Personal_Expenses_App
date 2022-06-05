1921f3ce11f5a0e563f551abc5ede83347a852a4
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-06-05 19:15:35 +0530

    style text

--- lib/main.dart
+++ lib/main.dart
@@ -60,19 +60,38 @@ class MyHomePage extends StatelessWidget {
                 children: [
                   Container(
                     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    child: Text(tx.amount.toString()),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
                     decoration: BoxDecoration(
                       border: Border.all(
-                        color: Colors.black,
+                        color: Colors.purple,
                         width: 2,
                       ),
                     ),
                     padding: EdgeInsets.all(10),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(tx.title),
-                      Text(tx.date.toString()),
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        tx.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
                     ],
                   ),
                 ],
