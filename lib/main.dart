37b79a885ffcee31793b5a530fa93cd41e7d92b2
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-06-05 18:55:27 +0530

    style container

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,15 @@ class MyHomePage extends StatelessWidget {
               child: Row(
                 children: [
                   Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
                     child: Text(tx.amount.toString()),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
                   ),
                   Column(
                     children: [
