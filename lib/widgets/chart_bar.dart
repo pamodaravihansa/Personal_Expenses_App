45c9df3b5d223ff411a93d60583a25ce86c02b86
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-07-23 16:36:26 +0530

    style bar chart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -16,7 +16,7 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        FittedBox(child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
         SizedBox(height: 4),
         Container(
           height: 60,
